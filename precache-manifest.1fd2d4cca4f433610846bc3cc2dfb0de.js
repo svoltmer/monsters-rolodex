f355a165dfb579cbac115d703f550010fee87947
voltmer1 <voltmer1@rose-hulman.edu>
2020-09-14 16:06:27 -0400

    Updates

---
+++ precache-manifest.1fd2d4cca4f433610846bc3cc2dfb0de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d658af828279a598ddef4a68f64ba4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fe6cd1dc9321e32d53dd",
+    "url": "/monsters-rolodex/static/css/main.dbb2ceed.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6cd1dc9321e32d53dd",
+    "url": "/monsters-rolodex/static/js/main.39f275df.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
